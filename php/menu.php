bfb9c88f564c0ef4d7b992400291d29c237d1a13
Axel Floquet-Trillot <axel.floquettrillot@ynov.com>
2016-11-10 13:17:22 +0100

    Changement en PHP

---
+++ php/menu.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href="#"><h1>Portfolio <small>Axel Floquet-Trillot</small></h1></a> </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.html">Présentation Personnelle <span class="sr-only">(current)</span></a></li>
+                <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Parcours<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="parcours-professionnel.html">Professionnel</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="parcours-personnel.html">Personnel</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Fichiers et Liens<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a target="_blank" href="files/CV_Axel_Floquet-Trillot.pdf">CV (PDF)</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="http://aps-paris5.fr">Site de l'APS</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
