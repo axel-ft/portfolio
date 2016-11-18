2b91bf926bc1653d77af1c7c8dddc220d30267ac
Axel Floquet-Trillot <axel.floquettrillot@ynov.com>
2016-11-18 13:08:20 +0100

    Commit Competences

--- js/competences.js
+++ js/competences.js
@@ -1,15 +1,11 @@
-function progressBar(idElement, valueMax) {
-    // progressbar.js@1.0.0 version is used
-    // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+function progressBar(idElement, valueMax, couleurFin) {
     var element = document.getElementById(idElement)
     var bar = new ProgressBar.Circle(element, {
         color: '#aaa',
-        // This has to be the same size as the maximum width to
-        // prevent clipping
         strokeWidth: 4,
         trailWidth: 1,
         easing: 'easeOut',
-        duration: 1400,
+        duration: 2000,
         text: {
             autoStyleContainer: false
         },
@@ -18,10 +14,9 @@ function progressBar(idElement, valueMax) {
             width: 1
         },
         to: {
-            color: '#10811d',
+            color: couleurFin,
             width: 4
         },
-        // Set default step function for all animate calls
         step: function (state, circle) {
             circle.path.setAttribute('stroke', state.color);
             circle.path.setAttribute('stroke-width', state.width);
@@ -41,9 +36,16 @@ function progressBar(idElement, valueMax) {
     bar.animate(valueMax); // Number from 0.0 to 1.0
 }
 
-progressBar('html5', 0.9);
-progressBar('css3', 0.8);
-progressBar('js', 0.5);
-progressBar('jquery', 0.6)
-progressBar('sql', 0.8);
-progressBar('php', 0.5);
+progressBar('html5', 0.9, '#339933');
+progressBar('css3', 0.8, '#66cc33');
+progressBar('js', 0.5, "#ffcc33");
+progressBar('jquery', 0.6, "#cccc33");
+progressBar('sql', 0.8, '#66cc33');
+progressBar('php', 0.5, '#ffcc33');
+
+progressBar('bash', 1.0, '#009933');
+progressBar('c', 0.6, '#cccc33');
+progressBar('cpp', 0.01, '#ff00000');
+progressBar('java', 0.6, '#cccc33');
+progressBar('python', 0.01, '#ff0000');
+progressBar('git', 0.8, '#66cc33');
